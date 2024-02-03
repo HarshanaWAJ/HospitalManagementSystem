ed453a2f5f25984784a17955010a56fec12c3034
HarshanaWAJ <janithharshana2019@gmail.com>
2024-02-03 12:32:07 +0530

    home page css

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container} from 'react-bootstrap';
+import '../css/About.css';
 
 export default function AboutPage() {
   return (
     <section id='about' className='about'>
-      <Container>
+      <Container className='about'>
         About Page
       </Container>
     </section>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,13 +9,17 @@ export default function HomePage() {
   return (
     <section id='home' className='home'>
       <Container className='home-page'>
-          <Container className='home-main-body'>
-            <Container className='bg-img'>
+        <h3>Welcome to Medi Care</h3>
+          <Container className='home-main-content-1'>
+            <Container className='bg-img' >
               <img src={Bgimg} alt="background" className='bg-anim-img'/>
             </Container>
             <Container className='content-1'>
-              <img src={Logo} alt="background" className='content-img'/>
-              <h3>Welcome to Medi Care,</h3>
+              <img src={Logo} alt="background" className='content-img' animate={{}}/>
+              <h4>"We prioritize your health and well-being"</h4>
+              <p>Madi Care prioritizes your health and well-being. 
+                Our hospital management system is intended to streamline and improve your healthcare experience.
+              </p>
             </Container>
           </Container>
       </Container>
