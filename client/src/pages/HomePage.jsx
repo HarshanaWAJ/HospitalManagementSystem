fa91a15c3ad937ce26cd6676a098eb9d43b31923
HarshanaWAJ <janithharshana2019@gmail.com>
2024-02-03 09:08:40 +0530

    home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,17 +3,19 @@ import '../css/home_page.css';
 import { Container} from 'react-bootstrap';
 
 import Bgimg from '../assets/images/imgs/bg-img-1.avif';
+import Logo from '../assets/images/icons/MediCare.ico';
 
 export default function HomePage() {
   return (
     <section id='home' className='home'>
       <Container className='home-page'>
           <Container className='home-main-body'>
-            <Container>
+            <Container className='bg-img'>
               <img src={Bgimg} alt="background" className='bg-anim-img'/>
             </Container>
             <Container className='content-1'>
-                <p>I Love You</p>
+              <img src={Logo} alt="background" className='content-img'/>
+              <h3>Welcome to Medi Care,</h3>
             </Container>
           </Container>
       </Container>
