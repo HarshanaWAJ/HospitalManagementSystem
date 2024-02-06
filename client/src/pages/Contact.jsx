052997a3da5563db821181d6e30c58c30222a3d2
HarshanaWAJ <janithharshana2019@gmail.com>
2024-02-06 22:57:25 +0530

    Contact Page Started

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container} from 'react-bootstrap';
+import '../css/Contact.css';
 
 export default function Contact() {
   return (
     <section id='contact' className='contact'>
       <Container className='contact'>
-        Contact Page
+        <div className="contact-header">Contact Us</div>
+        <div className="contact-content">
+          
+        </div>
       </Container>
     </section>
   )
